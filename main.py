f1f2ca31b031c3640639d560c9451d91d7a9ab11
ds1242 <david.shaw1242@gmail.com>
2022-10-18 11:26:20 -0600

    changed how files are opened and looped through within template to replace names

--- main.py
+++ main.py
@@ -7,17 +7,19 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+PLACEHOLDER = "[name]"
 
-# f = open("./Input/Letters/starting_letter.txt")
+
+with open("./Input/Names/invited_names.txt", "r") as names_file:
+    names = names_file.readLines()
+
 # print(f.readlines())
+with open("./Input/Letters/starting_letter.txt") as starting_letter:
+    letter_contents = starting_letter.read()
+    for name in names:
+        new_name = name.strip('\n')
+        new_letter = letter_contents.replace(PLACEHOLDER, new_name)
 
-f = open("./Input/Names/invited_names.txt", "r")
-# print(f.readlines())
 
-for word in f:
-    name = word.strip('\n')
-    letter = open("./Input/Letters/starting_letter.txt")
-    new_letter = letter.replace("[name]", name)
-    
 
 
